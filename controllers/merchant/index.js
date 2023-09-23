d7a2d510dc14d75e30546f050a368335b33357db
Sujit <mnamesujit@gmail.com>
2023-09-24 04:51:56 +0530

    update-product work

--- controllers/merchant/index.js
+++ controllers/merchant/index.js
@@ -91,8 +91,69 @@ const addProduct = async (req, res) => {
   }
 };
 
+const updateProduct = async (req, res) => {
+  // Checking if the user is authenticated and has the "Merchant" role
+  if (req.user && req.user.user_role === "Admin") {
+    try {
+      const productId = req.params.productId;
+      const {
+        product_name,
+        description,
+        selling_price,
+        discount,
+        stock_quantity,
+        category_id,
+        main_image_url 
+    } = req.body;
+
+      const { user_id } = req.user;
+
+      const query = "UPDATE products SET product_name = ?, description = ?, selling_price = ?, discount = ?, stock_quantity = ?, category_id = ?, main_image_url = ? WHERE product_id = ? AND merchant_id = ?"
+
+      const values =  [
+        product_name,
+        description,
+        selling_price,
+        discount,
+        stock_quantity,
+        category_id,
+        main_image_url,
+        productId,
+        user_id, // Merchant id
+      ]
+
+      // Update the product in the database
+      polls.query(query, values, (err, result) => {
+        if(err) {
+          console.error("Error executing SQL query:", err);
+          return res.status(500).json({ message: "Internal server error" });
+        }
+        // Check if the product was updated successfully
+        if (result.affectedRows > 0) {
+          res.status(200).json({ message: "Product updated successfully" });
+        } else {
+          res.status(404).json({
+            message: "Product not found or you are not authorized to update it.",
+          });
+        }
+        })
+  
+    } catch (err) {
+      console.error("Error updating product:", err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } else {
+    // User is not authorized to update products
+    res
+      .status(403)
+      .json({ message: "You are not authorized to update products." });
+  }
+};
+
+
 
 module.exports = {
   getProducts,
   addProduct,
+  updateProduct,
 };
--- routes/merchant/merchant.js
+++ routes/merchant/merchant.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getProducts, addProduct } = require('../../controllers/merchant/index')
+const { getProducts, addProduct, updateProduct, } = require('../../controllers/merchant/index')
 const check_auth = require('../../middleware/check_auth')
 
 
 router.get('/', check_auth, getProducts);
 router.post('/', check_auth, addProduct);
-
+router.put('/:productId', check_auth, updateProduct);
 module.exports = router
