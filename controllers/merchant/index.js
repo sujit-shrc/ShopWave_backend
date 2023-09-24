0b05ebd382bcddb36df5f0ef41653156802c232a
Sujit <mnamesujit@gmail.com>
2023-09-24 20:13:37 +0530

    delete product controller work

--- controllers/merchant/index.js
+++ controllers/merchant/index.js
@@ -2,21 +2,18 @@ const polls = require("../../config/db");
 
 const getProducts = async (req, res) => {
   // Check if the user is authenticated and has the "right" role
-  if (
-    req.user &&
-    (req.user.user_role === "Admin")
-  ) {
+  if (req.user && req.user.user_role === "Admin") {
     try {
       // Insert the new product into the database
-      const query = "SELECT * FROM products  WHERE merchant_id = ?"
-      const values = [req.user.user_id]
+      const query = "SELECT * FROM products  WHERE merchant_id = ?";
+      const values = [req.user.user_id];
       polls.query(query, values, (err, result) => {
         if (err) {
           console.error("Error executing SQL query:", err);
           return res.status(500).json({ message: "Internal server error" });
         }
         res.status(201).json({ products: result });
-      })
+      });
     } catch (err) {
       console.error("Error adding product:", err);
       res.status(500).json({ message: "Internal server error" });
@@ -34,13 +31,13 @@ const addProduct = async (req, res) => {
   if (req.user && req.user.user_role === "Admin") {
     try {
       const {
-          product_name,
-          description,
-          selling_price,
-          discount,
-          stock_quantity,
-          category_id,
-          main_image_url 
+        product_name,
+        description,
+        selling_price,
+        discount,
+        stock_quantity,
+        category_id,
+        main_image_url,
       } = req.body;
 
       // Check if any required field is missing
@@ -57,8 +54,9 @@ const addProduct = async (req, res) => {
 
       const { user_id } = req.user;
 
-      const query = "INSERT INTO products (merchant_id, product_name, description, selling_price, discount, stock_quantity, category_id, main_image_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-      
+      const query =
+        "INSERT INTO products (merchant_id, product_name, description, selling_price, discount, stock_quantity, category_id, main_image_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
       const values = [
         user_id, // Merchant id
         product_name,
@@ -68,17 +66,17 @@ const addProduct = async (req, res) => {
         stock_quantity,
         category_id,
         main_image_url,
-      ]
+      ];
 
       // Insert the new product into the database
       polls.query(query, values, (err, result) => {
-        if(err) {
+        if (err) {
           console.error("Error executing SQL query:", err);
           return res.status(500).json({ message: "Internal server error" });
         }
-         // Return the newly inserted product ID
+        // Return the newly inserted product ID
         res.status(201).json({ productId: result.insertId });
-      })
+      });
     } catch (err) {
       console.error("Error adding product:", err);
       res.status(500).json({ message: "Internal server error" });
@@ -103,14 +101,15 @@ const updateProduct = async (req, res) => {
         discount,
         stock_quantity,
         category_id,
-        main_image_url 
-    } = req.body;
+        main_image_url,
+      } = req.body;
 
       const { user_id } = req.user;
 
-      const query = "UPDATE products SET product_name = ?, description = ?, selling_price = ?, discount = ?, stock_quantity = ?, category_id = ?, main_image_url = ? WHERE product_id = ? AND merchant_id = ?"
+      const query =
+        "UPDATE products SET product_name = ?, description = ?, selling_price = ?, discount = ?, stock_quantity = ?, category_id = ?, main_image_url = ? WHERE product_id = ? AND merchant_id = ?";
 
-      const values =  [
+      const values = [
         product_name,
         description,
         selling_price,
@@ -120,11 +119,11 @@ const updateProduct = async (req, res) => {
         main_image_url,
         productId,
         user_id, // Merchant id
-      ]
+      ];
 
       // Update the product in the database
       polls.query(query, values, (err, result) => {
-        if(err) {
+        if (err) {
           console.error("Error executing SQL query:", err);
           return res.status(500).json({ message: "Internal server error" });
         }
@@ -133,11 +132,11 @@ const updateProduct = async (req, res) => {
           res.status(200).json({ message: "Product updated successfully" });
         } else {
           res.status(404).json({
-            message: "Product not found or you are not authorized to update it.",
+            message:
+              "Product not found or you are not authorized to update it.",
           });
         }
-        })
-  
+      });
     } catch (err) {
       console.error("Error updating product:", err);
       res.status(500).json({ message: "Internal server error" });
@@ -150,10 +149,46 @@ const updateProduct = async (req, res) => {
   }
 };
 
+const deleteProduct = async (req, res) => {
+  // Check if the user is authenticated and has the "Merchant" role
+  if (req.user && req.user.user_role === "Admin") {
+    try {
+      const productId = req.params.productId;
 
+      const query =
+        "DELETE FROM products WHERE product_id = ? AND merchant_id = ?";
+      const values = [productId, req.user.user_id];
+      // Delete the product from the database
+      polls.query(query, values, (err, result) => {
+        if (err) {
+          console.error("Error executing SQL query:", err);
+          return res.status(500).json({ message: "Internal server error" });
+        }
+        // Check if the product was deleted successfully
+        if (result.affectedRows > 0) {
+          res.status(200).json({ message: "Product deleted successfully" });
+        } else {
+          res.status(404).json({
+            message:
+              "Product not found or you are not authorized to delete it.",
+          });
+        }
+      });
+    } catch (err) {
+      console.error("Error deleting product:", err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } else {
+    // User is not authorized to delete products
+    res
+      .status(403)
+      .json({ message: "You are not authorized to delete products." });
+  }
+};
 
 module.exports = {
   getProducts,
   addProduct,
   updateProduct,
+  deleteProduct,
 };
--- routes/merchant/merchant.js
+++ routes/merchant/merchant.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getProducts, addProduct, updateProduct, } = require('../../controllers/merchant/index')
+const { getProducts, addProduct, updateProduct, deleteProduct, } = require('../../controllers/merchant/index')
 const check_auth = require('../../middleware/check_auth')
 
 
 router.get('/', check_auth, getProducts);
 router.post('/', check_auth, addProduct);
 router.put('/:productId', check_auth, updateProduct);
+router.delete('/:productId', check_auth, deleteProduct);
 module.exports = router
